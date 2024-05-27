c630e64ae2bc823a10c765d1cb016b89e0cd7850
AJStangl <ajstangl@gmail.com>
2024-05-27 11:43:49 -0400

    Adding install ps1.

---
+++ install.ps1
@@ -0,0 +1,11 @@
+echo ":: Installing Python Virtual Environment to working directory"
+
+$(python -m venv venv)
+
+echo ":: Activating Python Virtual Environment"
+.\venv\Scripts\activate
+
+echo ":: Installing Python packages"
+$(pip install -r requirements.txt)
+
+echo ":: Installation complete - run 'run.ps1' to execute the script."
